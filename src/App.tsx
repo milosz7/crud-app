40e5e4089b5d8fab1d431086d730cf216011b34e
Milosz Bargiel <miloszbargiel@gmail.com>
2022-06-08 20:48:07 +0200

    Install packages, add routes, remove unnecessary files and init store

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import Post from './components/pages/Post/Post';
+import Home from './components/pages/Home/Home';
+import Error from './components/pages/Error/Error';
+import Edit from './components/pages/Edit/Edit';
+import Add from './components/pages/Add/Add';
+import About from './components/pages/About/About';
 
-function App() {
+
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path='/' element={<Home />}></Route>
+      <Route path='post'>
+        <Route path='add' element={<Add />}></Route>
+        <Route path=':id' element={<Post />}></Route>
+        <Route path='edit/:id' element={<Edit />}></Route>
+      </Route>
+      <Route path='about' element={<About />}></Route>
+      <Route path='*' element={<Error />}></Route>
+    </Routes>
   );
 }
 
