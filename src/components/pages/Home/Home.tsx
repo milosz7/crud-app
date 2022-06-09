7d75749d87676f685c7f5589d6c8a5a1b0b82066
Milosz Bargiel <miloszbargiel@gmail.com>
2022-06-09 18:36:51 +0200

    Add header, footer and Home components

--- src/components/pages/Home/Home.tsx
+++ src/components/pages/Home/Home.tsx
@@ -1,6 +1,16 @@
+import { Button } from "react-bootstrap";
+import PostsList from "../../features/PostsList/PostsList";
+import { Link } from 'react-router-dom'
+
 const Home = () => {
   return (
-    <h1>Home</h1>
+    <div className="py-3">
+      <div className="d-flex justify-content-between mb-3">
+        <h2 className='m-0'>All posts</h2>
+        <Button variant="outline-info" as={Link as any} to='/post/add'>Add post</Button>
+      </div>
+      <PostsList />
+    </div>
   )
 };
 
---
+++ src/components/views/Footer/Footer.tsx
@@ -0,0 +1,11 @@
+
+
+const Footer = () => {
+  return (
+    <footer className="text-center text-secondary p-3">
+      Copyright © 2022 BlogApp
+    </footer>
+  )
+};
+
+export default Footer;
