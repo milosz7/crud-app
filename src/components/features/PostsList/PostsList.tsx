e1907afe21bc60e651adf8e8840c613b93e8d0f4
Milosz Bargiel <miloszbargiel@gmail.com>
2022-06-10 19:36:23 +0200

    Install react-router-bootstrap package

--- src/components/features/PostsList/PostsList.tsx
+++ src/components/features/PostsList/PostsList.tsx
@@ -1,7 +1,7 @@
 import { Col, Row, Card, Button } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
 import { selectAllPosts } from '../../../store/slices/postsSlice';
 import { useAppSelector } from '../../../store/hooks';
+import { LinkContainer } from 'react-router-bootstrap';
 
 const PostsList = () => {
   const posts = useAppSelector(selectAllPosts);
@@ -16,7 +16,9 @@ const PostsList = () => {
               <Card.Text className='m-0'><span className="fw-bold">Author: </span>{post.author}</Card.Text>
               <Card.Text ><span className="fw-bold">Published: </span>{post.publishedDate}</Card.Text>
               <Card.Text>{post.shortDescription}</Card.Text>
-              <Button as={Link as any} to={`post/${post.id}`} className='align-self-start mt-auto'>Read more</Button>
+              <LinkContainer to={`post/${post.id}`}>
+                <Button className='align-self-start mt-auto'>Read more</Button>
+              </LinkContainer>
             </Card.Body>
           </Card>
         </Col>
