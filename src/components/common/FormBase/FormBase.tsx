609f3ecd538c38728efb7cee80ae3f434a58affb
Milosz Bargiel <miloszbargiel@gmail.com>
2022-06-11 22:56:08 +0200

    create a FormBase component

---
+++ src/components/common/FormBase/FormBase.tsx
@@ -0,0 +1,48 @@
+import { Form } from 'react-bootstrap';
+import React from 'react';
+
+interface FormBaseData {
+  id: string;
+  type: string;
+  as?: 'textarea';
+  placeholder: string;
+  onChange: React.ChangeEventHandler<HTMLInputElement>;
+  isValid: boolean | undefined;
+  rows?: number | undefined;
+  title: string;
+}
+
+const FormBase = ({ id, type, as, placeholder, onChange, isValid, rows, title }: FormBaseData) => {
+  if (as)
+    return (
+      <Form.Group className="mb-3">
+        <Form.Label htmlFor={id}>{title}</Form.Label>
+        <Form.Control
+          type={type}
+          rows={rows}
+          as={as}
+          placeholder={placeholder}
+          id={id}
+          onChange={onChange}
+          isValid={isValid}
+          required
+        ></Form.Control>
+      </Form.Group>
+    );
+  return (
+    <Form.Group className="mb-3">
+      <Form.Label htmlFor={id}>{title}</Form.Label>
+      <Form.Control
+        type={type}
+        className="w-50"
+        placeholder={placeholder}
+        id={id}
+        onChange={onChange}
+        isValid={isValid}
+        required
+      ></Form.Control>
+    </Form.Group>
+  );
+};
+
+export default FormBase;
