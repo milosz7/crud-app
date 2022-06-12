2de8b979adbc83ab044b17bb616964b4542b15cd
Milosz Bargiel <miloszbargiel@gmail.com>
2022-06-12 20:56:58 +0200

    Add form component to Edit page

--- src/components/pages/Edit/Edit.tsx
+++ src/components/pages/Edit/Edit.tsx
@@ -1,6 +1,11 @@
+import EditPostForm from "../../features/EditPostForm/EditPostForm";
+
 const Edit = () => {
   return (
-    <h1>Edit</h1>
+    <div className="py-3 m-auto mt-3 col-lg-8 col-10">
+      <h2>Edit Post</h2>
+      <EditPostForm />
+    </div>
   )
 };
 
